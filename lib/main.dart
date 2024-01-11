eb74565c0818d060f72d6c91f0236fc7c6a0a4eb
flutter project <faisalakmal450@gmail.com>
2024-01-11 18:36:38 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+// ignore: unused_import
+import 'package:uangkoo/pages/home_page.dart';
+import 'package:uangkoo/pages/main_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: MainPage(), theme: ThemeData(primarySwatch: Colors.green));
+  }
+}
